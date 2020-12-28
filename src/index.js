9df2d342aeb4ebfcba69d7cb8bef6a57dbcb517c
Kirill Borun <kbroun@yandex.by>
2020-12-28 15:20:12 +0300

    part4

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header'
+import SearchPanel from './components/search-panel'
+import TodoList from './components/todo-list'
 
-const TodoList = () => {
-  return (
-    <ul>
-     <li>Learn REact</li>
-     <li>Build Awesome App</li>
-    </ul>
-  );
-};
-
-const AppHeader = () => {
-  return(
-    <h1>My Todo List</h1>
-  );
-};
-
-const SearchPanel = () =>{
-  return (
-    <input placeholder='search' />
-  );
-};
 
 const App = () => {
   return(
