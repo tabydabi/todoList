376148308ca2a795660f498541282bb8568a29fa
Kirill Borun <kbroun@yandex.by>
2020-12-30 14:13:59 +0300

    add CSS*boo

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import './search-panel.css'
 
 const SearchPanel = () =>{
     return (
-      <input placeholder='search' />
+      <input type='text'
+      className='form-control search-input'
+       placeholder='type to search' />
     );
   };
 
--- src/index.js
+++ src/index.js
@@ -3,14 +3,27 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header'
 import SearchPanel from './components/search-panel'
 import TodoList from './components/todo-list'
+import TodoListItem from './components/todo-list-item'
+import ItemStatusFilter from './components/item-status-filter';
 
 
 const App = () => {
+
+  const todoData = [
+    {label: 'Drink Cofee', important: false, id:1},
+    {label: 'Make aweasome app', important: true, id:2},
+    {label: 'have a lunch', important: false, id:3}
+  ]
+
   return(
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
     </div>
   )
 }
