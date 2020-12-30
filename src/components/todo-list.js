4130d76c14f99c45cd564dbac8bb52137eed5ca0
Kirill Borun <kbroun@yandex.by>
2020-12-30 14:14:36 +0300

    add Css(bootstrap)

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TodoListItem from './todo-list-item'
 import './todo-list.css'
 const TodoList = ({todos}) => {
-
   const elements = todos.map((item) => {
 
     const {id, ...itemProps} = item;
